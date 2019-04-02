5a9f8bd7bfbd2757f473a0a312f0c1dba4a6f306
Samuel Santana <ifnmg@192.168.163.38>
2019-04-01 22:06:55 -0300

    TESTE

---
+++ src/TestaConta.java
@@ -0,0 +1,19 @@
+
+public class TestaConta {
+    
+    public static void main(String[] args){
+    
+    Aluno A1 = new Aluno();
+    A1.alunos = "Samuel";  
+    System.out.println("Aluno " +A1.alunos);
+    
+    Professor P1 = new Professor();
+    P1.professores = "Luciano";  
+    System.out.println("Professor " +P1.professores);
+    
+    Aula Al1 = new Aula();
+    Al1.aulas = "POO I"; 
+    System.out.println("Aulas " +Al1.aulas);
+    } 
+      
+}
